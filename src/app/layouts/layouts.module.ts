47ed40ce3ad62c9aba96a84bcf2467148272f0b4
ANDRIATSIFERANA <andriatsiferana.nomena@gmail.com>
2021-11-03 23:05:13 +0300

    init project structure

---
+++ src/app/layouts/layouts.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { PagesLayoutComponent } from '@layouts/pages-layout/pages-layout.component';
+
+@NgModule({
+  declarations: [
+    PagesLayoutComponent
+  ],
+  imports: [
+  ],
+})
+export class LayoutsModule { }
