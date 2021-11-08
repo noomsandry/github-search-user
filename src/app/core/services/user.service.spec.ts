eb1d24198f904b6693284b996e5f59fc75398bb1
ANDRIATSIFERANA <andriatsiferana.nomena@gmail.com>
2021-11-08 22:32:56 +0300

    service test

--- src/app/core/services/user.service.spec.ts
+++ src/app/core/services/user.service.spec.ts
@@ -1,11 +1,12 @@
 import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
+import { SearchUserCriteria } from '@core/interfaces/user.interface';
 import { StoreModule } from '@ngrx/store';
-
 import { UserService } from './user.service';
+import * as _ from 'lodash';
 
 describe('UserService', () => {
-  let service: UserService;
+  let userService: UserService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -14,10 +15,32 @@ describe('UserService', () => {
         HttpClientModule
       ]
     });
-    service = TestBed.inject(UserService);
+    userService = TestBed.inject(UserService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(userService).toBeTruthy();
+  });
+
+  it('should find user', () => {
+    const criteria: SearchUserCriteria = {
+      q: 'noomsandry',
+    }
+
+    userService.searchUser(criteria).subscribe((response) => {
+      expect(_.find(response.users, user => user.login === criteria.q)).toBeTruthy()
+    })
+  });
+
+  it('number of result per page', () => {
+    const criteria: SearchUserCriteria = {
+      q: 'eric',
+      per_page: 20,
+      page: 1
+    }
+
+    userService.searchUser(criteria).subscribe((response) => {
+      expect(response.users.length).toEqual(criteria.per_page);
+    })
   });
 });
